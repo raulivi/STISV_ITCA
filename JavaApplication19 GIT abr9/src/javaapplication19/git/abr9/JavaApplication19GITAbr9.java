b1c89ed4adaa913c5d254068451c8a912fe588c1
pc <pc@172.16.12.109>
2017-04-09 10:52:40 -0600

    Inicio de proyecto

--- JavaApplication19 GIT abr9/src/javaapplication19/git/abr9/JavaApplication19GITAbr9.java
+++ JavaApplication19 GIT abr9/src/javaapplication19/git/abr9/JavaApplication19GITAbr9.java
@@ -1,14 +1,7 @@
 package javaapplication19.git.abr9;
 
-/**
- *
- * @author pc
- */
 public class JavaApplication19GITAbr9 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
     }
